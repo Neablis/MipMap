2339690736efa46cd14ef8fc1bdadc1ebbe55226
Mitchell DeMarco <Neablis121@gmail.com>
2019-05-18 11:30:31 -0700

    Update script.js

--- MipMap/assets/js/script.js
+++ MipMap/assets/js/script.js
@@ -1,24 +1,24 @@
-$(document).ready( function() {
-	var tempImage = new Image();
-	tempImage.src = "assets/images/ku-xlarge.png";
-	var mMipMap = new MipMap( tempImage  );
-	var toExample = $("#example"); 
-	var toContainer = $("#bottom"); 
+$(document).ready(function() {
+    var tempImage = new Image();
+    tempImage.src = "assets/images/ku-xlarge.png";
+    var mMipMap = new MipMap(tempImage);
+    var toExample = $("#example");
+    var toContainer = $("#bottom");
 
-	var tiHeight = 0;
-	var tiWidth = 0;
-	
-	function resizeImage( ){
-		var padding = 5;
-		toExample[0].src = mMipMap.GetImage( tiWidth-padding ).src;
-	}
-	
-	window.onresize = function(){
-		tiHeight = toContainer.height();
-		tiWidth = toContainer.width();
-		resizeImage(); 
-	};
-	
-    document.addEventListener( "MipMapLoaded", window.onresize, false); 
+    var tiHeight = 0;
+    var tiWidth = 0;
+
+    function resizeImage() {
+        var padding = 5;
+        toExample[0].src = mMipMap.GetImage(tiWidth - padding).src;
+    }
+
+    window.onresize = function() {
+        tiHeight = toContainer.height();
+        tiWidth = toContainer.width();
+        resizeImage();
+    };
+
+    document.addEventListener("MipMapLoaded", window.onresize, false);
 
 });
